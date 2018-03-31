9a0089495788be9887071b2b3d9f694fe55a07ae
Leandro Santos <leandro.ricardo.cba@gmail.com>
2018-03-31 09:52:43 -0400

    Version 0.0.1 - Starting application

---
+++ src/main/java/com/hotel/torontoplace/TorontoplaceApplication.java
@@ -0,0 +1,12 @@
+package com.hotel.torontoplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorontoplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorontoplaceApplication.class, args);
+	}
+}
